2e73910486711cd8e639a6eb4261bd990f499953
eelco <eelco@7b491191-dbf0-0310-aff6-d879d4d69008>
2001-12-10 21:32:08 +0000

    fully fixed :)
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@356 7b491191-dbf0-0310-aff6-d879d4d69008

--- pipe.c
+++ pipe.c
@@ -59,7 +59,7 @@ char *read_header(unsigned long *blksize)
           allocated_blocks++;
           trace (TRACE_DEBUG,"read_header(): mem current: [%d] reallocated to [%d]",
                   usedmem, allocated_blocks*HEADER_BLOCK_SIZE);
-          memtst(((char *)realloc(header,allocated_blocks*HEADER_BLOCK_SIZE))==NULL);
+          memtst((header = (char *)realloc(header,allocated_blocks*HEADER_BLOCK_SIZE))==NULL);
       }
       /* now we concatenate all we have to the header */
 		if (strblock)
