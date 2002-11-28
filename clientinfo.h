cc93ae6df360360e7942c9707b345cfc9b479a38
roel <roel@7b491191-dbf0-0310-aff6-d879d4d69008>
2002-11-28 19:04:44 +0000

    *** empty log message ***
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@626 7b491191-dbf0-0310-aff6-d879d4d69008

---
+++ clientinfo.h
@@ -0,0 +1,29 @@
+/* 
+ * clientinfo.h
+ *
+ * definition of clientinfo structure & initializing function prototypes
+ */
+
+#ifndef CLIENT_INFO_H
+#define CLIENT_INFO_H
+
+#include <stdio.h>
+#include "config.h"
+
+#define IPNUM_LEN 32
+
+typedef struct
+{
+  FILE *tx,*rx;
+  void *conn;               /* db connection */
+  char ip[IPNUM_LEN];       /* client IP-number */
+  field_t clientname;       /* resolved client ip */
+  char *timeoutMsg;
+  int  timeout;             /* server timeout (seconds) */
+  void *userData;
+} clientinfo_t;
+
+
+typedef clientinfo_t ClientInfo; /* FIXME alias shouldn't be necessary */
+
+#endif
