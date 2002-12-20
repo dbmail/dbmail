793703295b90fe8808d73d604f8695fa9386ba7b
roel <roel@7b491191-dbf0-0310-aff6-d879d4d69008>
2002-12-20 09:20:49 +0000

    *** empty log message ***
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@681 7b491191-dbf0-0310-aff6-d879d4d69008

--- user.c
+++ user.c
@@ -555,11 +555,12 @@ int do_show(char *name)
       quotum = auth_getmaxmailsize(userid);
       quotumused = db_get_quotum_used(userid);
 
+      quiet_printf("\n");
       quiet_printf("User ID         : %llu\n", userid);
       quiet_printf("Username        : %s\n", auth_get_userid(&userid));
       quiet_printf("Client ID       : %llu\n",cid);
-      quiet_printf("Max. mailboxsize: %.02lf MB\n",quotum/(1024.0*1024.0));
-      quiet_printf("Quotum used     : %.02lf MB (%.1lf%%)\n", quotumused/(1024.0*1024.0), 
+      quiet_printf("Max. mailboxsize: %.02lf MB\n",(double)quotum/(1024.0*1024.0));
+      quiet_printf("Quotum used     : %.02lf MB (%.01lf%%)\n", (double)quotumused/(1024.0*1024.0), 
 		   (100.0 * quotumused)/quotum);
       quiet_printf("\n");
 
