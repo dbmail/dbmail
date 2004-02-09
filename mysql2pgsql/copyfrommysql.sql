d6f35b73d42a0a5050304f574039b618563906c5
ilja <ilja@7b491191-dbf0-0310-aff6-d879d4d69008>
2004-02-09 11:15:07 +0000

    removing the mysql2pgsql directory.
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@949 7b491191-dbf0-0310-aff6-d879d4d69008

--- mysql2pgsql/copyfrommysql.sql
+++
@@ -1,7 +0,0 @@
-# loads all data from mysql tables into temporary files
-
-SELECT * FROM users INTO OUTFILE '/tmp/users.mysqldata';
-SELECT * FROM aliases INTO OUTFILE '/tmp/aliases.mysqldata';
-SELECT * FROM mailboxes INTO OUTFILE '/tmp/mailboxes.mysqldata';
-SELECT * FROM messageblks INTO OUTFILE '/tmp/messageblks.mysqldata';
-SELECT * FROM messages INTO OUTFILE '/tmp/messages.mysqldata';
--- mysql2pgsql/pgsql-convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/usr/local/pgsql/bin/dropdb dbmail
-/usr/local/pgsql/bin/createdb dbmail
-/usr/local/pgsql/bin/psql dbmail < ../sql/postgresql/no-constraint-tables.pgsql
-/usr/local/pgsql/bin/psql dbmail < copytopgsql.sql
-/usr/local/pgsql/bin/psql dbmail < ../sql/postgresql/add-constraints.pgsql
-
-
