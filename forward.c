a0f21ba3ba3cc8bafffd2db55a49a8e25c225d29
ilja <ilja@7b491191-dbf0-0310-aff6-d879d4d69008>
2004-03-09 12:14:59 +0000

    only send the header through the pipe to sendmail if there is no database message to be sent. If a message is in the database, just send that messages (which includes its headers)
    __FILE__ and __FUNCTION__ added to trace() calls.
    indented forward.c to K&R with indent -kr -i8
    
    
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@1003 7b491191-dbf0-0310-aff6-d879d4d69008

--- forward.c
+++ forward.c
@@ -47,128 +47,155 @@ extern struct list smtpItems;
  * FIXME: there is no detail in the error reporting,
  * so there's no way to tell *which* targets failed...
  * */
-int forward(u64_t msgidnr, struct list *targets, const char *from, const char *header, u64_t headersize UNUSED)
+int forward(u64_t msgidnr, struct list *targets, const char *from,
+	    const char *header, u64_t headersize UNUSED)
 {
 
-  struct element *target=NULL;
-  char *command=NULL;
-  FILE *pipe=NULL;
-  int err;
-  field_t sendmail;
-  char timestr[50];
-  time_t td;
-  struct tm tm;
- 
+	struct element *target = NULL;
+	char *command = NULL;
+	FILE *pipe = NULL;
+	int err;
+	field_t sendmail;
+	char timestr[50];
+	time_t td;
+	struct tm tm;
 
-  time(&td);              /* get time */
-  tm = *localtime(&td);   /* get components */
-  strftime(timestr, sizeof(timestr), "%a %b %e %H:%M:%S %Y", &tm);
-  
-  GetConfigValue("SENDMAIL", &smtpItems, sendmail);
-  if (sendmail[0] == '\0')
-    trace(TRACE_FATAL, "forward(): SENDMAIL not configured (see config file). Stop.");
-  
-  trace(TRACE_INFO, "forward(): delivering to [%ld] external addresses",
-		  list_totalnodes(targets));
+	time(&td);		/* get time */
+	tm = *localtime(&td);	/* get components */
+	strftime(timestr, sizeof(timestr), "%a %b %e %H:%M:%S %Y", &tm);
 
-  if (!msgidnr)
-    {
-      trace(TRACE_ERROR, "forward(): got NULL as message id number");
-      return -1;
-    }
-  
-  target = list_getstart (targets);
+	GetConfigValue("SENDMAIL", &smtpItems, sendmail);
+	if (sendmail[0] == '\0')
+		trace(TRACE_FATAL,
+		      "%s,%s: SENDMAIL not configured (see config file). "
+		      "Stop.", __FILE__, __FUNCTION__);
 
-  while (target != NULL)
-    {
-      if ((((char *)target->data)[0]=='|') || (((char *)target->data)[0]=='!'))
-	{
-        
-	  /* external pipe command */
-	  command = (char *)my_malloc(strlen((char *)(target->data)));
-	  if (!command)
-	    {
-	      trace(TRACE_ERROR,"forward(): out of memory");
-	      return -1;
-	    }
-	  strcpy (command, (char *)(target->data)+1); /* skip the pipe (|) sign */
-	}
-      else
-	{
-	  /* pipe to sendmail */
-	  command = (char *)my_malloc(strlen((char *)(target->data))+
-					    strlen(sendmail)+2); /* +2 for extra space and \0 */
-	  if (!command)
-	    {
-	      trace(TRACE_ERROR,"forward(): out of memory");
-	      return -1;
-	    }
+	trace(TRACE_INFO,
+	      "%s,%s: delivering to [%ld] external addresses",
+	      __FILE__, __FUNCTION__, list_totalnodes(targets));
 
-	  trace(TRACE_DEBUG, "forward(): allocated memory for external command call");
-	  sprintf (command, "%s %s", sendmail, (char *)(target->data));
+	if (!msgidnr) {
+		trace(TRACE_ERROR,
+		      "%s,%s: got NULL as message id number",
+		      __FILE__, __FUNCTION__);
+		return -1;
 	}
 
-      trace(TRACE_INFO, "forward(): opening pipe to command %s", command);
+	target = list_getstart(targets);
 	
-      pipe = popen(command, "w"); /* opening pipe */
-      my_free (command);
-      command = NULL;
+	while (target != NULL) {
+		if ((((char *) target->data)[0] == '|')
+		    || (((char *) target->data)[0] == '!')) {
 
-      if (pipe != NULL)
-	{
-	  trace(TRACE_DEBUG, "forward(): call to popen() successfully opened pipe [%d]",
-			  fileno(pipe));
-			
-          if (((char *)target->data)[0]=='!')
-            {
-              /* ! tells us to prepend an mbox style header in this pipe */
-              trace(TRACE_DEBUG, "forward(): appending mbox style from header to pipe returnpath : %s", from);
-              /* format: From<space>address<space><space>Date */
-              fprintf (pipe, "From %s  %s\n", from, timestr);   
-            }
+			/* external pipe command */
+			command =
+				(char *) 
+				my_malloc(strlen((char *) (target->data)));
+			if (!command) {
+				trace(TRACE_ERROR,
+				      "%s,%s: out of memory",
+				      __FILE__, __FUNCTION__);
+				return -1;
+			}
+			/* skip the pipe (|) sign */
+			strcpy(command, (char *) (target->data) + 1);
+		} else {
+			/* pipe to sendmail */
+			command = (char *) 
+				my_malloc(strlen((char *)
+						 (target->data)) + 
+					  strlen(sendmail) + 2);
+			/* +2 for extra space and \0 */
+			if (!command) {
+				trace(TRACE_ERROR,
+				      "%s,%s: out of memory",
+				      __FILE__, __FUNCTION__);
+				return -1;
+			}
 
-          /* first send header if this is a direct pipe through */
-          fprintf (pipe, "%s", header);
-          trace(TRACE_DEBUG, "forward(): wrote header to pipe");  
+			trace(TRACE_DEBUG,
+			      "%s,%s: allocated memory for external "
+			      "command call", __FILE__, __FUNCTION__);
+			sprintf(command, "%s %s", sendmail,
+				(char *) (target->data));
+		}
 
-	  trace(TRACE_INFO, "forward(): sending message id number [%llu] to forward pipe", msgidnr);
-			
-          err = ferror(pipe);
+		trace(TRACE_INFO, "%s,%s: opening pipe to command %s",
+		      __FILE__, __FUNCTION__, command);
 
-          trace(TRACE_DEBUG, "forward(): ferror reports"
-              " %d, feof reports %d on pipe %d", err,
-              feof (pipe),
-              fileno (pipe));
+		pipe = popen(command, "w");	/* opening pipe */
+		my_free(command);
+		command = NULL;
 
-          if (!err)
-            {
-              if (msgidnr != 0)
-                {
-                  trace(TRACE_DEBUG, "forward(): sending lines from"
-                      "message %llu on pipe %d", msgidnr, fileno(pipe));
-                  db_send_message_lines (pipe, msgidnr, -2, 1);
-                }
-            }
+		if (pipe != NULL) {
+			trace(TRACE_DEBUG,
+			      "%s,%s: call to popen() successfully "
+			      "opened pipe [%d]", __FILE__, __FUNCTION__,
+			      fileno(pipe));
 
-	  trace(TRACE_DEBUG, "forward(): closing pipes");
+			if (((char *) target->data)[0] == '!') {
+				/* ! tells us to prepend an mbox style header in this pipe */
+				trace(TRACE_DEBUG,
+				      "%s,%s: appending mbox style from "
+				      "header to pipe returnpath : %s",
+				      __FILE__, __FUNCTION__, from);
+				/* format: From<space>address<space><space>Date */
+				fprintf(pipe, "From %s  %s\n", from,
+					timestr);
+			}
 
-          if (!ferror(pipe))
-            {
-              pclose (pipe);
-              trace(TRACE_DEBUG, "forward(): pipe closed");
-            }
-          else
-            {
-              trace(TRACE_ERROR, "forward(): error on pipe");
-            }
-        }
-      else 
-        {
-          trace(TRACE_ERROR, "forward(): Could not open pipe to [%s]", sendmail);
-        }
-      target = target->nextnode;
-    }
+			trace(TRACE_INFO,
+			      "%s,%s: sending message id number [%llu] "
+			      "to forward pipe", __FILE__, __FUNCTION__,
+			      msgidnr);
 
-  return 0;			
+			err = ferror(pipe);
+
+			trace(TRACE_DEBUG, "%s,%s: ferror reports"
+			      " %d, feof reports %d on pipe %d", __FILE__,
+			      __FUNCTION__, err, feof(pipe), fileno(pipe));
+
+			if (!err) {
+				if (msgidnr != 0) {
+					trace(TRACE_DEBUG,
+					      "%s,%s: sending lines from"
+					      "message %llu on pipe %d",
+					      __FILE__, __FUNCTION__,
+					      msgidnr, fileno(pipe));
+					db_send_message_lines(pipe,
+							      msgidnr, -2,
+							      1);
+				} else {
+					/* only send the headers if there
+					 * is no message to send. */
+					trace(TRACE_DEBUG,
+					      "%s,%s: writing header to "
+					      "pipe", __FILE__, __FUNCTION__);
+					fprintf(pipe, "%s", header);
+				}
+
+			}
+
+			trace(TRACE_DEBUG, "%s,%s: closing pipes",
+			      __FILE__, __FUNCTION__);
+
+			if (!ferror(pipe)) {
+				pclose(pipe);
+				trace(TRACE_DEBUG,
+				      "%s,%s: pipe closed",
+				      __FILE__, __FUNCTION__);
+			} else {
+				trace(TRACE_ERROR,
+				      "%s,%s: error on pipe",
+				      __FILE__, __FUNCTION__);
+			}
+		} else {
+			trace(TRACE_ERROR,
+			      "%s,%s: Could not open pipe to [%s]",
+			      __FILE__, __FUNCTION__, sendmail);
+		}
+		target = target->nextnode;
+	}
+
+	return 0;
 }
-
