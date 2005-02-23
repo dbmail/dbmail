b33fce55d579d28f7d39bd4cdb340a83d995ab84
paul <paul@7b491191-dbf0-0310-aff6-d879d4d69008>
2005-02-23 19:17:36 +0000

    added some example scripts
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@1613 7b491191-dbf0-0310-aff6-d879d4d69008

--- contrib/sql2sql/mysql_fill_is_header.sh
+++ contrib/sql2sql/mysql_fill_is_header.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# $Id$
+#
+# very fast script for fixing the is_header field
+#
+
 mysql --skip-column-names -B -e "select dbmail_messageblk_idnr from messageblks group by physmessage_id" dbmail |\
 	awk 'BEGIN { printf("\nupdate dbmail_messageblks set is_header=1 where messageblk_idnr in ("); }   { if(NR % 200 == 0) { printf("\nupdate dbmail_messageblks set is_header=1 where messageblk_idnr in ("); i=0; } else { printf("%s,",$1); }}' |\
 	sed 's/,$/);/'
