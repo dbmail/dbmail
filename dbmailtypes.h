8a25e8e9afcae4428eb349eaa2b19464171c2aed
Paul J Stevens <paul@nfg.nl>
2006-05-23 14:51:36 +0200

    resync

--- dbmailtypes.h
+++ dbmailtypes.h
@@ -1,5 +1,5 @@
 /*
- $Id: dbmailtypes.h 2096 2006-04-30 18:39:56Z aaron $
+ $Id: dbmailtypes.h 2129 2006-05-23 12:51:32Z paul $
 
  Copyright (C) 1999-2004 IC & S  dbmail@ic-s.nl
  Copyright (c) 2005-2006 NFG Net Facilities Group BV support@nfg.nl
