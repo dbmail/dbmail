e1f7a515b305cf1fe7e9860b710b8202e2e3ddb8
Paul J Stevens <paul@nfg.nl>
2006-06-01 20:55:50 +0200

    resync

--- dbmail-message.c
+++ dbmail-message.c
@@ -1,5 +1,5 @@
 /*
-  $Id: dbmail-message.c 2139 2006-05-27 17:27:06Z paul $
+  $Id: dbmail-message.c 2140 2006-06-01 18:55:45Z paul $
 
   Copyright (c) 2004-2006 NFG Net Facilities Group BV support@nfg.nl
 
