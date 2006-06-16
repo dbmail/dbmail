c8114b35f10741210522cb20cb70a87277e000f5
Paul J Stevens <paul@nfg.nl>
2006-06-16 14:14:26 +0200

    resync

--- check_dbmail_message.c
+++ check_dbmail_message.c
@@ -17,7 +17,7 @@
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  *
- *  $Id: check_dbmail_message.c 2157 2006-06-06 11:30:48Z paul $ 
+ *  $Id: check_dbmail_message.c 2174 2006-06-16 12:14:23Z paul $ 
  *
  *
  *  
