062914ef0615f2d731d1fd970edf0a3c71f77b53
Paul J Stevens <paul@nfg.nl>
2006-10-21 09:50:50 +0200

    resync

--- imap4.h
+++ imap4.h
@@ -18,7 +18,7 @@
  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-/* $Id: imap4.h 2308 2006-10-20 13:36:50Z paul $
+/* $Id: imap4.h 2313 2006-10-21 07:50:51Z paul $
  * 
  * imap4.h
  */
