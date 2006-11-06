42d92414197f769002b8dbf7b852ca2efec05833
Paul J Stevens <paul@nfg.nl>
2006-11-06 20:00:50 +0100

    resync

--- db.c
+++ db.c
@@ -1,4 +1,4 @@
-/* $Id: db.c 2354 2006-11-04 19:06:24Z paul $ */
+/* $Id: db.c 2358 2006-11-06 19:00:51Z paul $ */
 /*
   Copyright (C) 1999-2004 IC & S  dbmail@ic-s.nl
   Copyright (c) 2005-2006 NFG Net Facilities Group BV support@nfg.nl
@@ -22,7 +22,7 @@
 /**
  * \file db.c
  * 
- * $Id: db.c 2354 2006-11-04 19:06:24Z paul $
+ * $Id: db.c 2358 2006-11-06 19:00:51Z paul $
  *
  * implement database functionality. This used to split out
  * between MySQL and PostgreSQL, but this is now integrated. 
