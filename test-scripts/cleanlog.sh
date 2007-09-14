224a055903cebab7b3cb13a764211808ef745c76
Paul Stevens <paul@nfg.nl>
2007-09-14 12:43:44 +0000

    fix cleanlog script
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@2760 7b491191-dbf0-0310-aff6-d879d4d69008

--- test-scripts/cleanlog.sh
+++ test-scripts/cleanlog.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-
-grep -E 'COMMAND|RESPONSE' - | \
-	cut -f5,8- -d' ' | \
-	sed -e 's/COMMAND/C/' -e 's/RESPONSE/S/' -e 's,dbmail/imap4d,,'
+sed 's/\r//'|grep -E 'COMMAND|RESPONSE' -|cut -b17-|cut -d' ' -f2,5-|sed -e 's/COMMAND/C/' -e 's/RESPONSE/S/' -e 's,dbmail/imap4d,,'
