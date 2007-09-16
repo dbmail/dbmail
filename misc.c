271993ca85fc1ad7d9a298b7fff532f585c64a5d
paul <paul@7b491191-dbf0-0310-aff6-d879d4d69008>
2007-09-16 18:23:15 +0000

    fix regression
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/branches/dbmail_2_2_branch@2765 7b491191-dbf0-0310-aff6-d879d4d69008

--- misc.c
+++ misc.c
@@ -2160,7 +2160,6 @@ char * dbmail_iconv_str_to_utf8(const char* str_in, const char *charset)
 char * dbmail_iconv_str_to_db(const char* str_in, const char *charset)
 {
 	char * subj=NULL;
-	size_t l;
 	iconv_t conv_iconv;
 
 	dbmail_iconv_init();
@@ -2168,11 +2167,8 @@ char * dbmail_iconv_str_to_db(const char* str_in, const char *charset)
 	if ( str_in == NULL )
 		return NULL;
 	
-	if ( (l = strlen(str_in)) == 0 )
-		return NULL;
-	
-	if (! g_mime_utils_text_is_8bit((unsigned char *)str_in, l) )
-		return g_strndup(str_in, l);
+	if (! g_mime_utils_text_is_8bit((unsigned char *)str_in, strlen(str_in)) )
+		return g_strdup(str_in);
 
 	if ((subj=g_mime_iconv_strdup(ic->to_db,str_in)) != NULL)
 		return subj;
