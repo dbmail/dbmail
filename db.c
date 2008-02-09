7b5186e6204ef02998fb1427661c1e6decfb3236
paul <paul@7b491191-dbf0-0310-aff6-d879d4d69008>
2008-02-09 12:08:23 +0000

    use outer join instead of subselect during icheck_physmessages
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/branches/dbmail_2_2_branch@2935 7b491191-dbf0-0310-aff6-d879d4d69008

--- db.c
+++ db.c
@@ -1543,12 +1543,14 @@ int db_icheck_physmessages(gboolean cleanup)
 
 	if (cleanup) {
 		snprintf(query, DEF_QUERYSIZE, 
-			"DELETE FROM %sphysmessage WHERE id NOT IN "
-			"(SELECT physmessage_id FROM %smessages)", DBPFX, DBPFX);
+			"DELETE FROM %sphysmessage p "
+			"LEFT JOIN %smessages m ON p.id = m.physmessage_id "
+			"WHERE m.message_idnr IS NULL ", DBPFX, DBPFX);
 	} else {
 		snprintf(query, DEF_QUERYSIZE, 
-			"SELECT COUNT(*) FROM %sphysmessage WHERE id NOT IN "
-			"(SELECT physmessage_id FROM %smessages)", DBPFX, DBPFX);
+			"SELECT COUNT(*) FROM %sphysmessage p "
+			"LEFT JOIN %smessages m ON p.id = m.physmessage_id "
+			"WHERE m.message_idnr IS NULL ", DBPFX, DBPFX);
 	}
 
 	if ((result = db_query(query)) < 0) {
