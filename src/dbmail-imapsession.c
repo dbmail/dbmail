c2274fc9ca666db3ff3ce5523cc4668a05dd77c8
Paul Stevens <paul@nfg.nl>
2008-05-25 15:16:30 +0000

    fix a couple of regressions in the message retrieval code
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@3017 7b491191-dbf0-0310-aff6-d879d4d69008

--- src/dbmail-imapsession.c
+++ src/dbmail-imapsession.c
@@ -29,7 +29,7 @@
 
 #define THIS_MODULE "imapsession"
 #define BUFLEN 2048
-#define SEND_BUF_SIZE 8192
+#define SEND_BUF_SIZE 1024
 #define MAX_ARGS 512
 #define IDLE_TIMEOUT 30
 
@@ -67,16 +67,15 @@ static void send_data(ImapSession *self, MEM * from, int cnt)
 	char buf[SEND_BUF_SIZE];
 	size_t l;
 
-	while (cnt >= SEND_BUF_SIZE) {
+	for (cnt -= SEND_BUF_SIZE; cnt >= 0; cnt -= SEND_BUF_SIZE) {
 		memset(buf,0,sizeof(buf));
-		l = mread(buf, SEND_BUF_SIZE-1, from);
+		l = mread(buf, SEND_BUF_SIZE, from);
 		dbmail_imap_session_printf(self, "%s", (l>0)?buf:"");
-		cnt -= l;
 	}
 
-	if (cnt > 0) {
+	if (cnt < 0) {
 		memset(buf,0,sizeof(buf));
-		l = mread(buf, cnt, from);
+		l = mread(buf, cnt + SEND_BUF_SIZE, from);
 		dbmail_imap_session_printf(self, "%s", (l>0)?buf:"");
 	}
 }
