2bc1dc218b79bcaeec3eb2883c89578e7cd24c66
Paul Stevens <paul@nfg.nl>
2008-08-24 08:32:21 +0000

    move DbmailMailbox
    git-svn-id: https://svn.ic-s.nl/svn/dbmail/trunk/dbmail@3054 7b491191-dbf0-0310-aff6-d879d4d69008

--- src/dbmail-mailbox.h
+++ src/dbmail-mailbox.h
@@ -33,6 +33,36 @@
 #define _DBMAIL_MAILBOX_H
 
 #include "dbmail.h"
+
+typedef struct {
+	u64_t id;
+	u64_t rows;		// total number of messages in mailbox
+	u64_t recent;
+	u64_t unseen;
+	u64_t owner_id;
+	u64_t size;
+
+	gchar *name;
+
+	GList *sorted;		// ordered list of UID values
+
+	MailboxInfo *info;	// cache mailbox metadata;
+	GTree *msginfo; 	// cache MessageInfo
+
+	GTree *found;		// search result (key: uid, value: msn)
+	GTree *ids; 		// key: uid, value: msn
+	GTree *msn; 		// key: msn, value: uid
+
+	GNode *search;
+	gchar *charset;		// charset used during search/sort
+
+	fetch_items_t *fi;	// imap fetch
+
+	gboolean uid, no_select, no_inferiors, no_children;
+
+} DbmailMailbox;
+
+
 DbmailMailbox * dbmail_mailbox_new(u64_t id);
 int dbmail_mailbox_open(DbmailMailbox *self);
 int dbmail_mailbox_sort(DbmailMailbox *self);
--- src/dbmailtypes.h
+++ src/dbmailtypes.h
@@ -537,35 +537,6 @@ typedef struct {
 } MessageInfo;
 
 
-typedef struct {
-	u64_t id;
-	u64_t rows;		// total number of messages in mailbox
-	u64_t recent;
-	u64_t unseen;
-	u64_t owner_id;
-	u64_t size;
-
-	gchar *name;
-
-	GList *sorted;		// ordered list of UID values
-
-	MailboxInfo *info;	// cache mailbox metadata;
-	GTree *msginfo; 	// cache MessageInfo
-
-	GTree *found;		// search result (key: uid, value: msn)
-	GTree *ids; 		// key: uid, value: msn
-	GTree *msn; 		// key: msn, value: uid
-
-	GNode *search;
-	gchar *charset;		// charset used during search/sort
-
-	fetch_items_t *fi;	// imap fetch
-
-	gboolean uid, no_select, no_inferiors, no_children;
-
-} DbmailMailbox;
-
-
 /*************************************************************************
 *                                 SIEVE
 *************************************************************************/
