503627621fa2b3376177e5305098276557faa8a1
Paul J Stevens <paul@nfg.nl>
2009-12-28 23:45:47 +0100

    fix copyright

--- src/server.c
+++ src/server.c
@@ -1,6 +1,6 @@
 /*
   
- Copyright (C) 1999-2004 IC & S  dbmail@D-s.nl
+ Copyright (C) 1999-2004 IC & S  dbmail@ic-s.nl
  Copyright (c) 2004-2009 NFG Net Facilities Group BV support@nfg.nl
 
  This program is free software; you can redistribute it and/or 
