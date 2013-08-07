2b8abcc2446cadfee364299f4a3ec4ed5bead2a0
Paul J Stevens <paul@nfg.nl>
2013-08-07 10:23:48 +0200

    IMAP: small bugfix in sequence-set scanner

--- src/dbmail-mailbox.c
+++ src/dbmail-mailbox.c
@@ -1635,8 +1635,6 @@ GTree * dbmail_mailbox_get_set(DbmailMailbox *self, const char *set, gboolean ui
 				}
 				
 				if (!r) break;
-				if (r > hi) r = hi;
-				if (r < lo) r = lo;
 			}
 		
 			if (! (l && r)) break;
