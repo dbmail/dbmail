7545d10435b83fb6e437d679d49089e87b1695ae
Paul J Stevens <paul@nfg.nl>
2013-08-19 10:21:16 +0200

    POP: plug file-descriptor leak
    regression introduced in b9da933bebb082c36cf060d43ddd146fd74b4e2a

--- src/clientsession.c
+++ src/clientsession.c
@@ -141,6 +141,9 @@ void client_session_bailout(ClientSession_T **session)
 
 	assert(c);
 
+	while (client_wbuf_len(c->ci) && (! (c->ci->client_state & CLIENT_ERR))) {
+		ci_write_cb(c->ci);
+	}
 	ci_cork(c->ci);
 
 	TRACE(TRACE_DEBUG,"[%p]", c);
