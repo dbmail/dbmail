dbmail/dbmail

commit b9da933bebb082c36cf060d43ddd146fd74b4e2a

POP/LMTP/TIMSIEVE: cleanup session shutdown
hopefully this fixes the pop3 crashes where the bailout sequence was
called twice under certain conditions, leading to stack corruption and
segmentation faults.

src/clientsession.c
@@ -32,18 +32,23 @@ extern struct event_base *evbase;
 ClientSession_T * client_session_new(client_sock *c)
 {
 	ClientBase_T *ci;
+	Mempool_T pool = c->pool;
 
 	char unique_id[UID_SIZE];
-
-	ClientSession_T * session = mempool_pop(c->pool, sizeof(ClientSession_T));
-	session->pool = c->pool;
 
 	if (c)
 		ci = client_init(c);
 	else
 		ci = client_init(NULL);
 
+	ClientSession_T * session = mempool_pop(pool, sizeof(ClientSession_T));
+
 	session->state = CLIENTSTATE_INITIAL_CONNECT;
+	session->pool = pool;
+	session->args = p_list_new(pool);
+	session->from = p_list_new(pool);
+	session->rbuff = p_string_new(pool, "");
+	session->messagelst = p_list_new(pool);
 
 	gethostname(session->hostname, sizeof(session->hostname));
 
@@ -57,10 +62,6 @@ ClientSession_T * client_session_new(client_sock *c)
 	ci_cork(ci);
 
 	session->ci = ci;
-	session->rbuff = p_string_new(session->pool, "");
-	session->messagelst = p_list_new(session->pool);
-	session->args = p_list_new(session->pool);
-	session->from = p_list_new(session->pool);
 
 	return session;
 }
@@ -138,7 +139,7 @@ void client_session_bailout(ClientSession_T **session)
 	List_T rcpt = NULL;
 	List_T messagelst = NULL;
 
-	if (! c) return;
+	assert(c);
 
 	ci_cork(c->ci);
 
@@ -147,7 +148,7 @@ void client_session_bailout(ClientSession_T **session)
 	if (server_conf->no_daemonize == 1) _exit(0);
 
 	client_session_reset(c);
-	c->state = CLIENTSTATE_ANY;
+	c->state = CLIENTSTATE_QUIT_QUEUED;
 	ci_close(c->ci);
 
 	p_string_free(c->rbuff, TRUE);
@@ -212,12 +213,12 @@ void client_session_bailout(ClientSession_T **session)
 	pool = c->pool;
 	mempool_push(pool, c, sizeof(ClientSession_T));
 	mempool_close(&pool);
+	c = NULL;
 }
 
 void client_session_read(void *arg)
 {
 	ClientSession_T *session = (ClientSession_T *)arg;
-	TRACE(TRACE_DEBUG, "[%p] state: [%d]", session, session->state);
 	ci_read_cb(session->ci);
 
 	uint64_t have = p_string_len(session->ci->read_buffer);
@@ -242,14 +243,9 @@ void client_session_read(void *arg)
 
 void client_session_set_timeout(ClientSession_T *session, int timeout)
 {
-	if (session && (session->state > CLIENTSTATE_ANY) && session->ci && session->ci->timeout) {
-		int current = session->ci->timeout->tv_sec;
-		if (timeout != current) {
-			ci_cork(session->ci);
-			session->ci->timeout->tv_sec = timeout;
-			ci_uncork(session->ci);
-		}
-	}
+	int current = session->ci->timeout->tv_sec;
+	if (timeout != current)
+		session->ci->timeout->tv_sec = timeout;
 }
 
 void socket_read_cb(int fd UNUSED, short what UNUSED, void *arg)
@@ -264,16 +260,20 @@ void socket_read_cb(int fd UNUSED, short what UNUSED, void *arg)
 void socket_write_cb(int fd UNUSED, short what UNUSED, void *arg)
 {
 	ClientSession_T *session = (ClientSession_T *)arg;
-	TRACE(TRACE_DEBUG,"[%p] state: [%d]", session, session->state);
 
-	if (session->ci->cb_write)
-		session->ci->cb_write(session);
+	if (session->state == CLIENTSTATE_QUIT_QUEUED)
+		return;
+
+	if (! session->ci->cb_write)
+		return;
+
+	session->ci->cb_write(session);
 
 	switch(session->state) {
 		case CLIENTSTATE_INITIAL_CONNECT:
 		case CLIENTSTATE_NON_AUTHENTICATED:
-			TRACE(TRACE_DEBUG,"reset timeout [%d]", server_conf->login_timeout);
-			client_session_set_timeout(session, server_conf->login_timeout);
+			//TRACE(TRACE_DEBUG,"reset timeout [%d]", server_conf->login_timeout);
+			//client_session_set_timeout(session, server_conf->login_timeout);
 			break;
 
 		case CLIENTSTATE_AUTHENTICATED:
@@ -282,14 +282,15 @@ void socket_write_cb(int fd UNUSED, short what UNUSED, void *arg)
 			client_session_set_timeout(session, server_conf->timeout);
 			break;
 
-		default:
-		case CLIENTSTATE_ANY:
-			break;
-
 		case CLIENTSTATE_LOGOUT:
 		case CLIENTSTATE_QUIT:
 		case CLIENTSTATE_ERROR:
 			client_session_bailout(&session);
+			break;
+
+		default:
+		case CLIENTSTATE_ANY:
+		case CLIENTSTATE_QUIT_QUEUED:
 			break;
 
 

src/pop3.c
@@ -186,6 +186,7 @@ static void pop3_close(ClientSession_T *session)
 	TRACE(TRACE_DEBUG,"[%p] sessionResult [%d]", session, session->SessionResult);
 
 	session->state = CLIENTSTATE_QUIT;
+
 	if (session->username != NULL && (session->was_apop || session->password != NULL)) {
 
 		switch (session->SessionResult) {
@@ -248,11 +249,11 @@ static void pop3_handle_input(void *arg)
 void pop3_cb_write(void *arg)
 {
 	ClientSession_T *session = (ClientSession_T *)arg;
-	TRACE(TRACE_DEBUG, "[%p] state: [%d]", session, session->state);
+	int state = session->state;
 
-	switch (session->state) {
+	switch (state) {
+		case CLIENTSTATE_QUIT_QUEUED:
 		case CLIENTSTATE_QUIT:
-			client_session_bailout(&session);
 			break;
 		default:
 			ci_write_cb(session->ci);
@@ -265,6 +266,7 @@ void pop3_cb_time(void * arg)
 {
 	ClientSession_T *session = (ClientSession_T *)arg;
 	session->state = CLIENTSTATE_QUIT;
+
 	ci_write(session->ci, "-ERR I'm leaving, you're too slow\r\n");
 }
 
@@ -276,7 +278,6 @@ static void reset_callbacks(ClientSession_T *session)
 
         UNBLOCK(session->ci->rx);
         UNBLOCK(session->ci->tx);
-
 	ci_uncork(session->ci);
 }
 
@@ -284,9 +285,10 @@ int pop3_handle_connection(client_sock *c)
 {
 	ClientSession_T *session = client_session_new(c);
 	session->state = CLIENTSTATE_INITIAL_CONNECT;
+
 	client_session_set_timeout(session, server_conf->login_timeout);
-        reset_callbacks(session);
         send_greeting(session);
+        reset_callbacks(session);
 	return 0;
 }
 
@@ -319,7 +321,9 @@ static int _pop3_session_authenticated(ClientSession_T *session, int user_idnr)
 {
 	ClientBase_T *ci = session->ci;
 	int result = 0;
+
 	session->state = CLIENTSTATE_AUTHENTICATED;
+
 	ci_authlog_init(ci, THIS_MODULE, (const char *)session->username, AUTHLOG_ACT);
 	client_session_set_timeout(session, server_conf->timeout);
 
@@ -371,6 +375,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 	TRACE(TRACE_DEBUG, "incoming buffer: [%s]", s);
 	if (! strlen(s)) return 1;
 
+	int state = session->state;
 	/* check for command issued */
 	/*
 	while (strchr(ValidNetworkChars, s[indx++]))
@@ -388,7 +393,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		if (strlen(value) == 0)
 			value = NULL;	/* no value specified */
 		else {
-			TRACE(TRACE_DEBUG, "state[%d], command issued :cmd [%s], value [%s]\n", session->state, command, value);
+			TRACE(TRACE_DEBUG, "state[%d], command issued :cmd [%s], value [%s]\n", state, command, value);
 		}
 	}
 
@@ -429,12 +434,13 @@ int pop3(ClientSession_T *session, const char *buffer)
 		 * the connection, commits all changes, and sends the final
 		 * "OK" message indicating that QUIT has completed. */
 		session->state = CLIENTSTATE_LOGOUT;
+
 		session->SessionResult = 0;
 		pop3_close(session);
 		return 0;
 		
 	case POP3_STLS:
-		if (session->state != CLIENTSTATE_INITIAL_CONNECT)
+		if (state != CLIENTSTATE_INITIAL_CONNECT)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 		if (! server_conf->ssl)
 			return pop3_error(session, "-ERR server error\r\n");
@@ -446,7 +452,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return 1;
 
 	case POP3_USER:
-		if (session->state != CLIENTSTATE_INITIAL_CONNECT)
+		if (state != CLIENTSTATE_INITIAL_CONNECT)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		if (session->username != NULL) {
@@ -465,7 +471,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return 1;
 
 	case POP3_PASS:
-		if (session->state != CLIENTSTATE_INITIAL_CONNECT)
+		if (state != CLIENTSTATE_INITIAL_CONNECT)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		if (session->password != NULL) {
@@ -505,7 +511,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return 1;
 
 	case POP3_LIST:
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		session->messagelst = p_list_first(session->messagelst);
@@ -546,7 +552,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return 1;
 
 	case POP3_STAT:
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		ci_write(ci, "+OK %" PRIu64 " %" PRIu64 "\r\n", 
@@ -558,7 +564,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 	case POP3_RETR:
 		TRACE(TRACE_DEBUG, "RETR command, retrieving message");
 
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		session->messagelst = p_list_first(session->messagelst);
@@ -585,7 +591,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return pop3_error(session, "-ERR [%s] no such message\r\n", value);
 
 	case POP3_DELE:
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		session->messagelst = p_list_first(session->messagelst);
@@ -608,7 +614,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return pop3_error(session, "-ERR [%s] no such message\r\n", value);
 
 	case POP3_RSET:
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		session->messagelst = p_list_first(session->messagelst);
@@ -631,7 +637,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return 1;
 
 	case POP3_LAST:
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		session->messagelst = p_list_first(session->messagelst);
@@ -654,14 +660,14 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return 1;
 
 	case POP3_NOOP:
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		ci_write(ci, "+OK\r\n");
 		return 1;
 
 	case POP3_UIDL:
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		session->messagelst = p_list_first(session->messagelst);
@@ -706,7 +712,7 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return 1;
 
 	case POP3_APOP:
-		if (session->state != CLIENTSTATE_INITIAL_CONNECT)
+		if (state != CLIENTSTATE_INITIAL_CONNECT)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		/* find out where the md5 hash starts */
@@ -779,12 +785,12 @@ int pop3(ClientSession_T *session, const char *buffer)
 		return 1;
 
 	case POP3_AUTH:
-		if (session->state != CLIENTSTATE_INITIAL_CONNECT)
+		if (state != CLIENTSTATE_INITIAL_CONNECT)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 		return pop3_error(session, "-ERR no AUTH mechanisms supported\r\n");
 
 	case POP3_TOP:
-		if (session->state != CLIENTSTATE_AUTHENTICATED)
+		if (state != CLIENTSTATE_AUTHENTICATED)
 			return pop3_error(session, "-ERR wrong command mode\r\n");
 
 		/* find out how many lines they want */

src/clientbase.c
@@ -141,10 +141,11 @@ ClientBase_T * client_init(client_sock *c)
 {
 	int serr;
 	ClientBase_T *client;
+	Mempool_T pool = c->pool;
 
-	client           = mempool_pop(c->pool, sizeof(ClientBase_T));
-	client->pool     = c->pool;
-	client->timeout  = mempool_pop(client->pool, sizeof(struct timeval));
+	client           = mempool_pop(pool, sizeof(ClientBase_T));
+	client->pool     = pool;
+	client->timeout  = mempool_pop(pool, sizeof(struct timeval));
 	client->sock     = c;
 	client->cb_error = client_error_cb;
 
@@ -194,8 +195,8 @@ ClientBase_T * client_init(client_sock *c)
 		}
 	}
 
-	client->read_buffer = p_string_new(client->pool, "");
-	client->write_buffer = p_string_new(client->pool, "");
+	client->read_buffer = p_string_new(pool, "");
+	client->write_buffer = p_string_new(pool, "");
 	client->rev = NULL;
 	client->wev = NULL;
 

src/dbmail.h.in
@@ -259,4 +259,7 @@
 #define LOCK(a) g_static_rec_mutex_lock(a)
 #define UNLOCK(a) g_static_rec_mutex_unlock(a)
 
+#define PLOCK(a) if (pthread_mutex_lock(&(a))) { perror("pthread_mutex_lock failed"); }
+#define PUNLOCK(a) if (pthread_mutex_unlock(&(a))) { perror("pthread_mutex_unlock failed"); }
+
 #endif

src/dm_mempool.c
@@ -24,8 +24,6 @@
 #define THIS_MODULE "mempool"
 
 #define M Mempool_T
-#define POOL_LOCK(a) if (pthread_mutex_lock(&(a))) { perror("pthread_mutex_lock failed"); }
-#define POOL_UNLOCK(a) if (pthread_mutex_unlock(&(a))) { perror("pthread_mutex_unlock failed"); }
 
 #define fail_unless(a) assert(a)
 
@@ -68,9 +66,9 @@ M mempool_open(void)
 void * mempool_pop(M MP, size_t blocksize)
 {
 	int error;
-	POOL_LOCK(MP->lock);
+	PLOCK(MP->lock);
 	void *block = mpool_calloc(MP->pool, 1, blocksize, &error);
-	POOL_UNLOCK(MP->lock);
+	PUNLOCK(MP->lock);
 	if (error != MPOOL_ERROR_NONE)
 		TRACE(TRACE_ERR, "%s", mpool_strerror(error));
 	return block;
@@ -79,9 +77,9 @@ void * mempool_pop(M MP, size_t blocksize)
 void * mempool_resize(M MP, void *block, size_t oldsize, size_t newsize)
 {
 	int error;
-	POOL_LOCK(MP->lock);
+	PLOCK(MP->lock);
 	void *newblock = mpool_resize(MP->pool, block, oldsize, newsize, &error);
-	POOL_UNLOCK(MP->lock);
+	PUNLOCK(MP->lock);
 	if (error != MPOOL_ERROR_NONE)
 		TRACE(TRACE_ERR, "%s", mpool_strerror(error));
 	assert (error == MPOOL_ERROR_NONE);
@@ -91,12 +89,12 @@ void * mempool_resize(M MP, void *block, size_t oldsize, size_t newsize)
 void mempool_push(M MP, void *block, size_t blocksize)
 {
 	int error;
-	POOL_LOCK(MP->lock);
+	PLOCK(MP->lock);
 	if ((error = mpool_free(MP->pool, block, blocksize)) != MPOOL_ERROR_NONE)
 		TRACE(TRACE_ERR, "%s", mpool_strerror(error));
 
 	fail_unless(error == MPOOL_ERROR_NONE);
-	POOL_UNLOCK(MP->lock);
+	PUNLOCK(MP->lock);
 }
 
 void mempool_stats(M MP)
@@ -116,7 +114,7 @@ void mempool_close(M *MP)
 	int error;
 	M mp = *MP;
 	pthread_mutex_t lock = mp->lock;
-	POOL_LOCK(lock);
+	PLOCK(lock);
 	mpool_t *pool = mp->pool;
 	if (pool) {
 		mempool_stats(mp);
@@ -125,7 +123,7 @@ void mempool_close(M *MP)
 	} else {
 		free(mp);
 	}
-	POOL_UNLOCK(lock);
+	PUNLOCK(lock);
 	pthread_mutex_destroy(&lock);
 	mp = NULL;
 }

src/lmtp.c
@@ -121,11 +121,13 @@ static void lmtp_handle_input(void *arg)
 void lmtp_cb_write(void *arg)
 {
 	ClientSession_T *session = (ClientSession_T *)arg;
-	TRACE(TRACE_DEBUG, "[%p] state: [%d]", session, session->state);
+	int state = session->state;
 
-	switch (session->state) {
+	TRACE(TRACE_DEBUG, "[%p] state: [%d]", session, state);
+
+	switch (state) {
+		case CLIENTSTATE_QUIT_QUEUED:
 		case CLIENTSTATE_QUIT:
-			client_session_bailout(&session);
 			break;
 		default:
 			if (p_string_len(session->ci->write_buffer) > session->ci->write_buffer_offset) {
@@ -152,6 +154,7 @@ static void reset_callbacks(ClientSession_T *session)
 static void lmtp_rset(ClientSession_T *session, gboolean reset_state)
 {
 	int state = session->state;
+
 	client_session_reset(session);
 	if (reset_state)
 		session->state = CLIENTSTATE_AUTHENTICATED;
@@ -166,8 +169,8 @@ int lmtp_handle_connection(client_sock *c)
 {
 	ClientSession_T *session = client_session_new(c);
 	client_session_set_timeout(session, server_conf->login_timeout);
-	reset_callbacks(session);
         send_greeting(session);
+	reset_callbacks(session);
 	return 0;
 }
 
@@ -242,7 +245,9 @@ int lmtp_tokenizer(ClientSession_T *session, char *buffer)
 
 	if (session->command_type == LMTP_DATA) {
 		if (command) {
-			if (session->state != CLIENTSTATE_AUTHENTICATED) {
+			int state = session->state;
+
+			if (state != CLIENTSTATE_AUTHENTICATED) {
 				return lmtp_error(session, "550 Command out of sequence\r\n");
 			}
 			if (p_list_length(session->rcpt) < 1) {
@@ -278,6 +283,7 @@ int lmtp(ClientSession_T * session)
 	const char *class, *subject, *detail;
 	size_t tmplen = 0, tmppos = 0;
 	char *tmpaddr = NULL, *tmpbody = NULL, *arg;
+	int state = 0;
 
 	switch (session->command_type) {
 
@@ -360,7 +366,9 @@ int lmtp(ClientSession_T * session)
 		/* We need to LHLO first because the client
 		 * needs to know what extensions we support.
 		 * */
-		if (session->state != CLIENTSTATE_AUTHENTICATED) {
+		state = session->state;
+
+		if (state != CLIENTSTATE_AUTHENTICATED) {
 			ci_write(ci, "550 Command out of sequence.\r\n");
 			return 1;
 		} 
@@ -426,7 +434,9 @@ int lmtp(ClientSession_T * session)
 		return 1;
 
 	case LMTP_RCPT:
-		if (session->state != CLIENTSTATE_AUTHENTICATED) {
+		state = session->state;
+
+		if (state != CLIENTSTATE_AUTHENTICATED) {
 			ci_write(ci, "550 Command out of sequence.\r\n");
 			return 1;
 		} 

src/timsieve.c
@@ -120,11 +120,13 @@ void tims_cb_time(void * arg)
 void tims_cb_write(void *arg)
 {
 	ClientSession_T *session = (ClientSession_T *)arg;
-	TRACE(TRACE_DEBUG, "[%p] state: [%d]", session, session->state);
+	int state = session->state;
 
-	switch(session->state) {
+	TRACE(TRACE_DEBUG, "[%p] state: [%d]", session, state);
+
+	switch(state) {
+		case CLIENTSTATE_QUIT_QUEUED:
 		case CLIENTSTATE_QUIT:
-			client_session_bailout(&session);
 			break;
 		default:
 			if (p_string_len(session->ci->write_buffer) > session->ci->write_buffer_offset) {
@@ -153,8 +155,8 @@ int tims_handle_connection(client_sock *c)
 	ClientSession_T *session = client_session_new(c);
 	session->state = CLIENTSTATE_NON_AUTHENTICATED;
 	client_session_set_timeout(session, server_conf->login_timeout);
-	reset_callbacks(session);
 	send_greeting(session);
+	reset_callbacks(session);
 	return 0;
 }
 
@@ -302,6 +304,8 @@ int tims(ClientSession_T *session)
 	SortResult_T *sort_result = NULL;
 	ClientBase_T *ci = session->ci;
 
+	int state = session->state;
+
 	TRACE(TRACE_DEBUG,"[%p] [%d][%s]", session, session->command_type, commands[session->command_type]);
 	switch (session->command_type) {
 	case TIMS_LOUT:
@@ -382,7 +386,7 @@ int tims(ClientSession_T *session)
 		return 1;
 
 	case TIMS_PUTS:
-		if (session->state != CLIENTSTATE_AUTHENTICATED) {
+		if (state != CLIENTSTATE_AUTHENTICATED) {
 			ci_write(ci, "NO \"Please authenticate first.\"\r\n");
 			break;
 		}
@@ -437,7 +441,7 @@ int tims(ClientSession_T *session)
 		break;
 		
 	case TIMS_SETS:
-		if (session->state != CLIENTSTATE_AUTHENTICATED) {
+		if (state != CLIENTSTATE_AUTHENTICATED) {
 			ci_write(ci, "NO \"Please authenticate first.\"\r\n");
 			break;
 		}
@@ -469,7 +473,7 @@ int tims(ClientSession_T *session)
 		return 1;
 		
 	case TIMS_GETS:
-		if (session->state != CLIENTSTATE_AUTHENTICATED) {
+		if (state != CLIENTSTATE_AUTHENTICATED) {
 			ci_write(ci, "NO \"Please authenticate first.\"\r\n");
 			break;
 		}
@@ -498,7 +502,7 @@ int tims(ClientSession_T *session)
 		return 1;
 		
 	case TIMS_DELS:
-		if (session->state != CLIENTSTATE_AUTHENTICATED) {
+		if (state != CLIENTSTATE_AUTHENTICATED) {
 			ci_write(ci, "NO \"Please authenticate first.\"\r\n");
 			break;
 		}
@@ -520,7 +524,7 @@ int tims(ClientSession_T *session)
 		return 1;
 
 	case TIMS_SPAC:
-		if (session->state != CLIENTSTATE_AUTHENTICATED) {
+		if (state != CLIENTSTATE_AUTHENTICATED) {
 			ci_write(ci, "NO \"Please authenticate first.\"\r\n");
 			break;
 		}
@@ -534,7 +538,7 @@ int tims(ClientSession_T *session)
 		return 1;
 		
 	case TIMS_LIST:
-		if (session->state != CLIENTSTATE_AUTHENTICATED) {
+		if (state != CLIENTSTATE_AUTHENTICATED) {
 			ci_write(ci, "NO \"Please authenticate first.\"\r\n");
 			break;
 		}

commit d4de6586fec97320c1f5f4ceb67f16795c4c5ece

POP: plug file-descriptor leak
regression introduced in b9da933bebb082c36cf060d43ddd146fd74b4e2a

src/clientsession.c
@@ -141,6 +141,9 @@ void client_session_bailout(ClientSession_T **session)
 
 	assert(c);
 
+	while (client_wbuf_len(c->ci) && (! (c->ci->client_state & CLIENT_ERR))) {
+		ci_write_cb(c->ci);
+	}
 	ci_cork(c->ci);
 
 	TRACE(TRACE_DEBUG,"[%p]", c);

src/pop3.c
@@ -222,6 +222,8 @@ static void pop3_close(ClientSession_T *session)
 	} else {
 		ci_write(ci, "+OK see ya later\r\n");
 	}
+
+	client_session_bailout(&session);
 }
 
 