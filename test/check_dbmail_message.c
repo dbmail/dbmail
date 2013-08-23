5900feb4b0b81564695bd616df7ed0884f5a489a
Paul J Stevens <paul@nfg.nl>
2013-08-23 15:23:31 +0200

    add test-case

--- test/check_dbmail_message.c
+++ test/check_dbmail_message.c
@@ -373,7 +373,13 @@ START_TEST(test_dbmail_message_store)
 	//COMPARE(e,t);
 	g_free(e);
 	g_free(t);
-
+	//-----------------------------------------
+	m = message_init(multipart_alternative3);
+	e = dbmail_message_to_string(m);
+	t = store_and_retrieve(m);
+	COMPARE(e,t);
+	g_free(e);
+	g_free(t);
 }
 END_TEST
 
