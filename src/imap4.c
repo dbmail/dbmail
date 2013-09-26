abac5b58c94f270e534320ff264054bc79530b78
Paul J Stevens <paul@nfg.nl>
2013-09-26 17:02:53 +0200

    IMAP: always disable read-events during deferred cleanup

--- src/imap4.c
+++ src/imap4.c
@@ -141,6 +141,7 @@ void imap_cleanup_deferred(gpointer data)
 	ImapSession *session = (ImapSession *)D->session;
 	ClientBase_T *ci = session->ci;
 
+	if (ci->rev) event_del(ci->rev);
 	if (client_wbuf_len(ci) && (! (ci->client_state & CLIENT_ERR))) {
 		ci_write_cb(ci);
 		dm_queue_push(imap_cleanup_deferred, session, NULL);
