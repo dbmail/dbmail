1f0a76c50c596aa02ad7053845df893a2e025241
Paul J Stevens <paul@nfg.nl>
2013-12-13 10:29:25 +0100

    SIEVE: decode headers #952

--- src/modules/sortsieve.c
+++ src/modules/sortsieve.c
@@ -519,9 +519,11 @@ int sort_getscript(sieve2_context_t *s, void *my)
 
 int sort_getheader(sieve2_context_t *s, void *my)
 {
+#define HEADER_REPEAT 10
+#define HEADER_WIDTH 1024
 	struct sort_context *m = (struct sort_context *)my;
 	char *header;
-	char **bodylist;
+	char bodylist[HEADER_REPEAT][HEADER_WIDTH];
 	GList *headers;
 	unsigned i;
 
@@ -529,10 +531,14 @@ int sort_getheader(sieve2_context_t *s, void *my)
 	
 	headers = dbmail_message_get_header_repeated(m->message, header);
 	
-	bodylist = g_new0(char *,g_list_length(headers)+1);
+	memset(bodylist, 0, sizeof(bodylist));
+
 	i = 0;
-	while (headers) {
-		bodylist[i++] = (char *)headers->data;
+	while (headers && i < HEADER_REPEAT) {
+		char *decoded = dbmail_iconv_decode_text((char *)headers->data);
+		strncpy(bodylist[i++], decoded, HEADER_WIDTH-1);
+		g_free(decoded);
+
 		if (! g_list_next(headers))
 			break;
 		headers = g_list_next(headers);
@@ -547,7 +553,7 @@ int sort_getheader(sieve2_context_t *s, void *my)
 			header, bodylist[i]);
 	}
 
-	sieve2_setvalue_stringlist(s, "body", bodylist);
+	sieve2_setvalue_stringlist(s, "body", (char ** const)bodylist);
 
 	return SIEVE2_OK;
 }
