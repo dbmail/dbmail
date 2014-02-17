d08e9e57cdda0759016b488d923bd1390ce4348a
Paul J Stevens <paul@nfg.nl>
2014-02-17 16:52:00 +0100

    POP3: fix segfault; fixes bug #1043

--- src/pop3.c
+++ src/pop3.c
@@ -302,7 +302,6 @@ int pop3_error(ClientSession_T * session, const char *formatstring, ...)
 
 	if (session->error_count >= MAX_ERRORS) {
 		ci_write(ci, "-ERR too many errors\r\n");
-		client_session_bailout(&session);
 		return -3;
 	} 
 	va_start(ap, formatstring);
@@ -314,7 +313,6 @@ int pop3_error(ClientSession_T * session, const char *formatstring, ...)
 	g_free(s);
 
 	if (ci->client_state & CLIENT_ERR) {
-		client_session_bailout(&session);
 		return -3;
 	}
 
