693df62a18ca1782826f17972272fc6ea6c8f8ab
Paul J Stevens <paul@nfg.nl>
2014-03-16 15:11:18 +0100

    generalise dumpbt.sh script

--- test-scripts/dumpbt.sh
+++ test-scripts/dumpbt.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # generate a backtrace for busy worker threads
+PNAME=${1:-dbmail-imapd}
 
 # only look for thread with more that MAXCPU cpu load
-MAXCPU=20
+MAXCPU=${2:-20}
 
 # get the imap PID
-IMAPPID=`pidof dbmail-imapd`
+PID=`pidof $PNAME`
+
+[ -n "$PID" ] || { echo "Error: Unable to determine main process ID for $PNAME"; exit 1; }
+
+echo "generate backtrace for $PNAME($PID) with CPU > $MAXCPU"
 
 # find busy threads
-BUSY=`top -H -b -n1 -p $IMAPPID|grep 'pool'|awk '{if ($9 > $MAXCPU) print $1}'`
+BUSY=`top -H -b -n1 -p $PID|awk '{if ($9 > $MAXCPU) print $1}'`
 
 [ -n "$BUSY" ] || exit 0
 
@@ -20,7 +25,7 @@ for TID in $BUSY; do
 	# get a backtrace
 	gdb --batch -x $tmpfile -p $TID > \
 		/tmp/debug-dbmail-backtrace.$TID
-        echo "backtrace in " /tmp/debug-dbmail-backtrace.$TID
+        echo "backtrace for $PNAME in " /tmp/debug-dbmail-backtrace.$TID
 done
 rm -f $tmpfile
 
