2c20a771dfb0b798946436ed2dc8506036f78e24
Paul J Stevens <paul@nfg.nl>
2014-06-15 14:51:55 +0200

    add some more test messages

--- test/check_dbmail_message.c
+++ test/check_dbmail_message.c
@@ -259,6 +259,27 @@ START_TEST(test_dbmail_message_store)
 	g_free(e);
 	g_free(t);
 	//-----------------------------------------
+	m = message_init(multipart_message7);
+	e = dbmail_message_to_string(m);
+	t = store_and_retrieve(m);
+	COMPARE(e,t);
+	g_free(e);
+	g_free(t);
+	//-----------------------------------------
+	m = message_init(multipart_message8);
+	e = dbmail_message_to_string(m);
+	t = store_and_retrieve(m);
+	COMPARE(e,t);
+	g_free(e);
+	g_free(t);
+	//-----------------------------------------
+	m = message_init(multipart_message9);
+	e = dbmail_message_to_string(m);
+	t = store_and_retrieve(m);
+	COMPARE(e,t);
+	g_free(e);
+	g_free(t);
+	//-----------------------------------------
 	m = message_init(multipart_mixed);
 	e = dbmail_message_to_string(m);
 	t = store_and_retrieve(m);
@@ -370,7 +391,6 @@ START_TEST(test_dbmail_message_store)
 	COMPARE(e,t);
 	g_free(e);
 	g_free(t);
-
 }
 END_TEST
 
