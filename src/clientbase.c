2a7cc331c7e900d59fa4d9879d04d259875d7b66
Alan Hicks <ahicks@p-o.co.uk>
2025-10-01 18:26:57 +0100

    getnameinfo() lines reformatted

--- src/clientbase.c
+++ src/clientbase.c
@@ -152,19 +152,21 @@ ClientBase_T * client_init(client_sock *c)
 		client->tx		= STDOUT_FILENO;
 	} else {
 		/* server-side */
-		if ((serr = getnameinfo(&c->saddr, c->saddr_len, client->dst_ip, NI_MAXHOST,
-					client->dst_port, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV))) {
+		if ((serr = getnameinfo(&c->saddr, c->saddr_len, client->dst_ip,
+			NI_MAXHOST, client->dst_port,
+			NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV))) {
 			TRACE(TRACE_INFO, "getnameinfo::error [%s]", gai_strerror(serr));
 		}
 
 		/* client-side */
-		if ((serr = getnameinfo(&c->caddr, c->caddr_len, client->src_ip, NI_MAXHOST-1,
-					client->src_port, NI_MAXSERV-1, NI_NUMERICHOST | NI_NUMERICSERV))) {
+		if ((serr = getnameinfo(&c->caddr, c->caddr_len, client->src_ip,
+			NI_MAXHOST-1, client->src_port,
+			NI_MAXSERV-1, NI_NUMERICHOST | NI_NUMERICSERV))) {
 			TRACE(TRACE_INFO, "getnameinfo:error [%s]", gai_strerror(serr));
 		}
 		if (server_conf->resolveIP) {
-			if ((serr = getnameinfo(&c->caddr, c->caddr_len, client->clientname, NI_MAXHOST-1,
-						NULL, 0, NI_NAMEREQD))) {
+			if ((serr = getnameinfo(&c->caddr, c->caddr_len, client->clientname,
+				NI_MAXHOST-1, NULL, 0, NI_NAMEREQD))) {
 				TRACE(TRACE_INFO, "getnameinfo:error [%s]", gai_strerror(serr));
 			}
 			TRACE(TRACE_NOTICE, "incoming connection on [%s:%s] from [%s:%s (%s)]",
