c028e185719d9f4c52fe8014d8b82ffc47aa6a38
Alan Hicks <ahicks@p-o.co.uk>
2026-01-19 13:12:20 +0000

    Refactored as per tildeslash comment

--- src/dm_db.c
+++ src/dm_db.c
@@ -331,22 +331,17 @@ Connection_T db_con_get(void)
 {
 	int i=0, k=0; Connection_T c = NULL;
 	while (! c) {
-		c = ConnectionPool_getConnection(pool);
+		TRY
+			c = ConnectionPool_getConnectionOrException(pool);
+		CATCH(SQLException)
+			LOG_SQLERROR;
+			int pool_size = ConnectionPool_size(pool);
+			int pool_active = ConnectionPool_active(pool);
+			TRACE(TRACE_WARNING, "Connection pool: size [%i] active [%i].", pool_size, pool_active);
+		END_TRY;
 		if (c) break;
 		if((int)(i % 5)==0) {
 			TRACE(TRACE_ALERT, "Thread is having trouble obtaining a database connection. Try [%d]", i);
-			if (ConnectionPool_isFull(pool)) {
-				int pool_size = ConnectionPool_size(pool);
-				int pool_active = ConnectionPool_active(pool);
-				TRACE(TRACE_WARNING,
-					"Connection pool is full: size [%i] active [%i].",
-					pool_size, pool_active
-				);
-			} else {
-				// A database error occurred. This could be due
-				// to network issues or database unavailability
-				TRACE(TRACE_WARNING, "Database error: Unable to acquire a connection.");
-			}
 			k = ConnectionPool_reapConnections(pool);
 			TRACE(TRACE_INFO, "Database reaper closed [%d] stale connections", k);
 		}
